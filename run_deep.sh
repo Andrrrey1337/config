5f241bbff2d8f445668a4fdc304eab6fd651e3cd
Andrey Ryabkov <anryabickov@yandex.ru>
2025-09-09 23:08:35 +0300

    Этап 3. vfs(test)

---
+++ run_deep.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Запуск с глубокой VFS..."
+python3 main.py --vfs-path vfs_deep.xml --startup-script test_vfs.sh
---
+++ run_medium.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Запуск со средней VFS..."
+python3 main.py --vfs-path vfs_medium.xml --startup-script test_vfs.sh
