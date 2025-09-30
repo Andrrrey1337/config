0cfee49d68a24bf96b7176aae904b1295afead1d
Andrey Ryabkov <anryabickov@yandex.ru>
2025-09-30 22:09:01 +0300

    Stage 2(redone)

--- main.py
+++ main.py
@@ -3,6 +3,39 @@ import os
 import socket
 import shlex
 import sys
+import xml.etree.ElementTree
+import base64
+
+vfs_data = {}
+
+
+def load_vfs(path_xml):
+    global vfs_data
+    try:
+        tree = xml.etree.ElementTree.parse(path_xml)
+        root = tree.getroot()
+
+        parse_vfs(root, "")
+    except Exception as e:
+        print(f"Ошибка загрузки: {e}", file=sys.stderr)
+        sys.exit(1)
+
+
+def parse_vfs(node, path):
+    global vfs_data
+    for child in node:
+        name = child.get("name")
+        new_path = path + "/" + name
+        if child.tag == "dir":
+            vfs_data[new_path] = "dir"
+            parse_vfs(child, new_path)
+        elif child.tag == "file":
+            content = child.text
+            try:
+                content = base64.b64decode(content).decode("utf-8")
+            except Exception:
+                content = "Ошибка декодирования"
+            vfs_data[new_path] = content
 
 
 def get_prompt():
@@ -54,13 +87,9 @@ def interactive():
             cmd, args = parser(line)
             execute_command(cmd, args)
 
-        except KeyboardInterrupt:
-            print("\n(Для выхода введите 'exit')")
         except EOFError:
             print("\nВыход через EOF")
             break
-        except ValueError as e:
-            print(f"Ошибка ввода: {e}", file=sys.stderr)
         except Exception as e:
             print(f"Неожиданная ошибка: {e}", file=sys.stderr)
 
@@ -82,9 +111,6 @@ def script(script_path):
             cmd, args = parser(line)
             if not execute_command(cmd, args):
                 sys.exit(1)
-        except ValueError as e:
-            print(f"Ошибка ввода: {e}", file=sys.stderr)
-            sys.exit(1)
         except Exception as e:
             print(f"Неожиданная ошибка: {e}", file=sys.stderr)
             sys.exit(1)
@@ -97,6 +123,9 @@ args = parse.parse_args()
 
 print(f"VFS path: {args.vfs_path if args.vfs_path else 'не задан'}")
 print(f"Star script: {args.startup_script if args.startup_script else 'не задан'}")
+
+if args.vfs_path:
+    load_vfs(args.vfs_path)
 
 if args.startup_script:
     script(args.startup_script)
